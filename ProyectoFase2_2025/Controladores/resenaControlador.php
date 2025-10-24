c52ee7f4b5850c51285d164d7331c372cff684f2
Vladimir <uvcmsv@gmail.com>
2025-10-23 21:30:29 -0600

    Resena_y_modelo

---
+++ ProyectoFase2_2025/Controladores/resenaControlador.php
@@ -0,0 +1,23 @@
+<?php
+require_once("../Modelos/resenaModelo.php");
+
+$resenaModelo = new ResenaModelo();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $id_usuario = 1; 
+    $id_tecnico = $_POST['tecnico_id'];
+    $id_solicitud = 1;
+    $calificacion = $_POST['calificacion'];
+    $comentario = $_POST['comentario'];
+
+    $resultado = $resenaModelo->registrarResena($id_usuario, $id_tecnico, $id_solicitud, $calificacion, $comentario);
+
+    if ($resultado) {
+        header("Location: ../Vistas/resena_tecnico.php?exito=1");
+    } else {
+        header("Location: ../Vistas/resena_tecnico.php?error=1");
+    }
+    exit;
+}
+?>
+
