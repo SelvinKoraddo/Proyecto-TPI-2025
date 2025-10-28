641b590206f2000965b668173bbf002082dd29bc
Vladimir <uvcmsv@gmail.com>
2025-10-27 20:09:25 -0600

    agregado tecnicoControlador

---
+++ ProyectoFase2_2025/Controladores/tecnicoControlador.php
@@ -0,0 +1,25 @@
+<?php
+require_once("../Modelos/tecnicoModelo.php");
+
+$tecnicoModelo = new TecnicoModelo();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nombre = $_POST['nombre'];
+    $correo = $_POST['correo'];
+    $telefono = $_POST['telefono'];
+    $contrasena = $_POST['contrasena'];
+    $zona = $_POST['zona'];
+    $tarifa = $_POST['tarifa'];
+    $descripcion = $_POST['descripcion'];
+    $especialidades = isset($_POST['especialidades']) ? $_POST['especialidades'] : [];
+
+    $resultado = $tecnicoModelo->registrarTecnico($nombre, $correo, $contrasena, $telefono, $tarifa, $zona, $descripcion, $especialidades);
+
+    if ($resultado) {
+        header("Location: ../Vistas/RegistroTecnico.php?exito=1");
+    } else {
+        header("Location: ../Vistas/RegistroTecnico.php?error=1");
+    }
+    exit;
+}
+?>
